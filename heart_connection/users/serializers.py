b4aaedbd5375c7d81ad781edbeabba76958092c3
Ильин Артём <joker9099@mail.ru>
2023-06-27 20:23:36 +0300

    Добавлено наложение водяного знака на аватарку

--- heart_connection/users/serializers.py
+++ heart_connection/users/serializers.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+from PIL import Image
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import serializers
 from .models import CustomUser
 
@@ -11,13 +14,22 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         avatar = self.context['request'].FILES.get('avatar')
+        image = Image.open(avatar)
+        watermark = Image.open('media/watermark/watermark.png')
+        image.paste(watermark, (0, 0), watermark)
+        output = BytesIO()
+        image.save(output, format='PNG')
+        output.seek(0)
+        processed_avatar = InMemoryUploadedFile(
+            output, None, f'{avatar.name.split(".")[0]}_processed.png', 'image/png', output.tell(), None
+        )
 
         user = CustomUser(
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
             gender=validated_data['gender'],
-            avatar=avatar,
+            avatar=processed_avatar,
         )
         user.set_password(validated_data['password'])
         user.save()
